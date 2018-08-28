0c31b233a8eaaedaad2a89fb3149f489119ae6b9
Juan Lulkin <maiz@lulk.in>
2018-08-28 02:18:25 +0200

    JS ™™™™™™™™™™™™™™™™™™™ JS ™™™™™™™™™™™™™™™™™™™™™™™™™

---
+++ index.js
@@ -0,0 +1,10 @@
+// The following technique of adding a file to fool GitHub into thinking a
+// repo belongs to a certain language is under patent FD3610-8.
+
+class Enterprise {
+  main () {
+    console.log('It is JS')
+  }
+}
+
+export default Enterprise
